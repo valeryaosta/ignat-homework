87e95de328e5ec59f574051902fe0c696e574b33
gerasimenko.valery@gmail.com <gerasimenko.valery@gmail.com>
2021-06-10 00:30:49 +0300

    hw5

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Junior() {
+    return (
+        <div>
+            junior
+        </div>
+    )
+}
+
+export default Junior
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function JuniorPlus() {
+    return (
+        <div>
+            junior+
+        </div>
+    )
+}
+
+export default JuniorPlus
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
