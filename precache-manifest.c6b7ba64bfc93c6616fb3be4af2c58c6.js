5e2eb36f91002dad32757e7973716d55686a417d
gerasimenko.valery@gmail.com <gerasimenko.valery@gmail.com>
2021-06-09 23:41:04 +0300

    Updates

---
+++ precache-manifest.c6b7ba64bfc93c6616fb3be4af2c58c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce897977c3b81520001a2e4349baad86",
+    "url": "/ignat-homework/index.html"
+  },
+  {
+    "revision": "8ec229b1bcfb4943b078",
+    "url": "/ignat-homework/static/css/main.6641a29b.chunk.css"
+  },
+  {
+    "revision": "b07d8ec6f9d95ad64aaf",
+    "url": "/ignat-homework/static/js/2.4e15ced9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ignat-homework/static/js/2.4e15ced9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec229b1bcfb4943b078",
+    "url": "/ignat-homework/static/js/main.56cfec1e.chunk.js"
+  },
+  {
+    "revision": "3ef1f0f92e4e86aa8c28",
+    "url": "/ignat-homework/static/js/runtime-main.251ae6e9.js"
+  },
+  {
+    "revision": "05e602f40a41c64399c78789d2a38766",
+    "url": "/ignat-homework/static/media/checked.05e602f4.png"
+  }
+]);
